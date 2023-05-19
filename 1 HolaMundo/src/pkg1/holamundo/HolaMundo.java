1a20d82bcf61115e045cfc54e8f542414bd3575c
MariaFernandaSotoTraconis <fernanda.soto.traconis@upvm.edu.mx>
2023-05-19 00:09:17 -0600

    SE AGREGA HOLA MUNDO

---
+++ 1 HolaMundo/src/pkg1/holamundo/HolaMundo.java
@@ -0,0 +1,12 @@
+
+package pkg1.holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+       
+    System.out.println("HOLA MUNDO :3");
+    
+            }
+    
+}
