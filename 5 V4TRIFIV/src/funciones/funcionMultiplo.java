7d25fd0b7fbfdb9a7ad01907021571f89a92a78c
MariaFernandaSotoTraconis <fernanda.soto.traconis@upvm.edu.mx>
2023-05-26 09:44:53 -0600

    AGREGUE FUNCIONES

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiplo.java
@@ -0,0 +1,20 @@
+
+package funciones;
+
+public class funcionMultiplo {
+    public String retornaMultiplo(int num){
+          if(num == 0 && num == 0)
+          {
+           return("TRIFIV");
+          }
+          else if (num == 0)
+          {
+          return("TRI");
+          }
+          else if (num == 0)
+          {
+          return("FIV");
+          }
+          return ("Ninguno");
+}
+}
