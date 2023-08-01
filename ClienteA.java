f7b0c1e2db8335ff5759bbfd1be14927e9565686
MariaFernandaSotoTraconis <fernanda.soto.traconis@upvm.edu.mx>
2023-08-01 01:18:39 -0600

    Se agrega Unidad3

---
+++ ClienteA.java
@@ -0,0 +1,165 @@
+
+package com.app.sockets.test;
+
+import com.app.sockets.chat.Cliente;
+import com.app.sockets.chat.Servidor;
+import com.app.sockets.dao.ConversacionImpl;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClienteA extends javax.swing.JFrame implements Observer {
+ConversacionImpl conversacionDAO = new ConversacionImpl();
+
+    public ClienteA() {
+        initComponents();
+        this.getRootPane().setDefaultButton(this.btnEnviar);
+        this.setTitle("Cliente A");
+        Servidor servidor = new Servidor(5000); // el que deseas
+        servidor.addObserver(this);
+        Thread hilo = new Thread(servidor);
+        hilo.start();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        btnLimpiar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtMensaje = new javax.swing.JTextArea();
+        txtMensajeEnviar = new javax.swing.JTextField();
+        btnEnviar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+        jLabel1.setText("Ingresa tu usuario");
+
+        txtNombre.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+
+        btnLimpiar.setBackground(new java.awt.Color(218, 189, 248));
+        btnLimpiar.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarActionPerformed(evt);
+            }
+        });
+
+        txtMensaje.setColumns(20);
+        txtMensaje.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+        txtMensaje.setRows(5);
+        jScrollPane1.setViewportView(txtMensaje);
+
+        txtMensajeEnviar.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+
+        btnEnviar.setBackground(new java.awt.Color(214, 183, 245));
+        btnEnviar.setFont(new java.awt.Font("Bell MT", 0, 14)); // NOI18N
+        btnEnviar.setText("Enviar");
+        btnEnviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEnviarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnLimpiar, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtMensajeEnviar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnEnviar)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(9, 9, 9)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLimpiar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtMensajeEnviar, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
+                    .addComponent(btnEnviar))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarActionPerformed
+    String mensaje = this.txtNombre.getText() + " : " + this.txtMensajeEnviar.getText() + "\n";
+        this.txtMensaje.append(mensaje);
+        conversacionDAO.registrarConversacionA(mensaje);
+        Cliente cliente = new Cliente(6000, mensaje);
+        Thread hilo = new Thread(cliente);
+        hilo.start();        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEnviarActionPerformed
+
+    private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
+      limpiar();
+    }//GEN-LAST:event_btnLimpiarActionPerformed
+    private void limpiar(){
+        txtMensaje.setText("");
+        txtMensajeEnviar.setText("");
+        txtNombre.setText("");
+    }
+     
+    public static void main(String args[]) {
+    try {
+    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+    if ("Nimbus".equals(info.getName())) {
+    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+    break;
+    }
+    }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ClienteA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ClienteA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ClienteA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ClienteA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+          
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ClienteA().setVisible(true);
+            }
+        });
+    }
+
+  
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnEnviar;
+    private javax.swing.JButton btnLimpiar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea txtMensaje;
+    private javax.swing.JTextField txtMensajeEnviar;
+    private javax.swing.JTextField txtNombre;
+    // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void update(Observable o, Object arg) {
+         txtMensaje.append((String)arg);
+    }
+}
